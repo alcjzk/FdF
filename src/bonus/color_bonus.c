5cb9a92d1342a950e7c2c2e1bca4a2382975b41c
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-01-24 06:30:51 +0200

    Fix #4

--- src/bonus/color_bonus.c
+++ src/bonus/color_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 06:25:30 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/01/24 00:32:09 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/01/24 06:29:45 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,14 @@ t_color	color_from_str(const char *str)
 {
 	t_color		color;
 	int			value;
+	size_t		max;
 
+	max = 4;
 	color.value = 0;
 	if (str_startswith(str, "0x"))
 		str += 2;
 	value = hex_value(*(str++));
-	while (value != -1)
+	while (max-- > 0 && value != -1)
 	{
 		color.value <<= 4;
 		color.value |= value;
