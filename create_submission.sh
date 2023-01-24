3bdeea7ffecb394f9b49c2b38796b6f715dd1013
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-01-24 10:47:45 +0200

    Minor correction

--- create_submission.sh
+++ create_submission.sh
@@ -1,6 +1,6 @@
-make fclean &&
+make fclean
 git checkout --orphan submit &&
-git reset HEAD &&
+git reset &&
 rm -rf libft .vscode .github .gitmodules README.md create_submission.sh &&
 git clone https://github.com/alcjzk/libft.git libft &&
 rm -rf libft/.github libft/README.md libft/.git &&
