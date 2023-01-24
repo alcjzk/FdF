45c88dffd0771abf0e6f5e401d11fa3610c390ad
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-01-24 10:42:57 +0200

    Added create submission script

---
+++ create_submission.sh
@@ -0,0 +1,8 @@
+make fclean &&
+git checkout --orphan submit &&
+git reset HEAD &&
+rm -rf libft .vscode .github .gitmodules README.md create_submission.sh &&
+git clone https://github.com/alcjzk/libft.git libft &&
+rm -rf libft/.github libft/README.md libft/.git &&
+git add . &&
+git commit -m "Initial commit"
